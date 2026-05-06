42dc56cb838cbbf3ff6f150348c687fecd56bf14
BenjCripps <ben.cr@getjobber.com>
2026-05-06 10:09:32 -0700

    hero shot animation

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.body.classList.add('loaded');
+  animateHero();
 });
+
+function animateHero() {
+  const name = document.querySelector('.hero__name');
+  const tagline = document.querySelector('.hero__tagline');
+  const socials = document.querySelector('.socials');
+
+  requestAnimationFrame(() => {
+    name.classList.add('is-visible');
+
+    setTimeout(() => {
+      tagline.classList.add('is-visible');
+    }, 500);
+
+    setTimeout(() => {
+      socials.classList.add('is-visible');
+    }, 1100);
+  });
+}
